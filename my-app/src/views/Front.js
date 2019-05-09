9a22019b30816c0a150f6d997a12bdd495917b63
JohnnyJii <jukka.juntunen@metropolia.fi>
2019-05-09 11:49:00 +0300

    like

--- my-app/src/views/Front.js
+++ my-app/src/views/Front.js
@@ -15,7 +15,7 @@ const Front = (props) => {
       <React.Fragment>
         {/* <Table picArray={picArray}/> */}
         <Grid item sm={8}>
-          <Scrollbars class="scroll" style={{width: '100%', height: 365, backgroundColor: "#12355b"}}>
+          <Scrollbars class="scroll" style={{width: '100%', height: 550, backgroundColor: "#12355b"}}>
             <ImageGrid picArray={picArray}/>
           </Scrollbars>
         </Grid>
--- my-app/src/views/Likes.js
+++ my-app/src/views/Likes.js
@@ -1,38 +1,31 @@
 import React from 'react';
+import './css/Likes.css';
 
 class Likes extends React.Component {
-    constructor(props){
-        super(props)
-            this.state ={
-                likes: 0,
-                updated: false
-            }
-            this.updateLikes = this.updateLikes.bind(this);
-        }
-        updateLikes() {
-            if(!this.state.updated) {
-                this.setState((prevState, props) => {
-                    return {
-                        likes: prevState.likes +1,
-                        updated: true
-                    };
-                });
-            } else {
-                this.setState((prevState, props) => {
-                    return {
-                        likes: prevState.likes -1,
-                        updated: false
-                    };
-                });
-            }
-        }
-        render() {
-            return (
-                <div class="Like">
-                    <p onClick={this.updateLikes}>Like</p>
-                    <p>{this.state.likes}</p>
-                </div>
-            );
-        }
+    constructor() {
+        super();
+        this.state = {
+            liked: false };
+
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick() {
+        this.setState({
+            liked: !this.state.liked });
+
+    }
+
+    render() {
+        const text = this.state.liked ? '' : '';
+        const label = this.state.liked ? 'Unlike' : 'Liked';
+        return (
+            React.createElement("div", { className: "customContainer" },
+                React.createElement("button", { className: "btn btn-primary", onClick: this.handleClick },
+                    label), React.createElement("p", null, "you ",
+                    text, " ")));
+
+
+
+    }}
 export default Likes;

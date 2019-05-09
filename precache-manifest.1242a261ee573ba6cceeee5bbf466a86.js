15ea55f326a1b4b02834fa071aaf0d21ab86fcb3
JohnnyJii <jukka.juntunen@metropolia.fi>
2019-05-09 13:36:50 +0300

    Updates

---
+++ precache-manifest.1242a261ee573ba6cceeee5bbf466a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b5934b623f3c6f5377f221959d77982",
+    "url": "/VillePellosniemi/react_projekti/static/media/giphy.0b5934b6.gif"
+  },
+  {
+    "revision": "d7c883a64f268fd39024",
+    "url": "/VillePellosniemi/react_projekti/static/js/runtime~main.5e6ac163.js"
+  },
+  {
+    "revision": "656c42fcda3783e479db",
+    "url": "/VillePellosniemi/react_projekti/static/js/main.904ed210.chunk.js"
+  },
+  {
+    "revision": "6f43b7b3544d9f58d2ac",
+    "url": "/VillePellosniemi/react_projekti/static/js/2.e2d5cc73.chunk.js"
+  },
+  {
+    "revision": "656c42fcda3783e479db",
+    "url": "/VillePellosniemi/react_projekti/static/css/main.a43ecde0.chunk.css"
+  },
+  {
+    "revision": "6f43b7b3544d9f58d2ac",
+    "url": "/VillePellosniemi/react_projekti/static/css/2.39be9536.chunk.css"
+  },
+  {
+    "revision": "148cc5096d91c2680fe291f09fb6d77d",
+    "url": "/VillePellosniemi/react_projekti/index.html"
+  }
+];
